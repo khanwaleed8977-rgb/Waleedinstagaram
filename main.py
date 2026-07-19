48601431348578e59f017b3ae9859d5176799b38
khanwaleed8977-rgb <khanwaleed8977@gmail.com>
2026-07-20 00:34:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -167,4 +167,5 @@ def monitor():
 
 # ====================== مین ======================
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=9000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
